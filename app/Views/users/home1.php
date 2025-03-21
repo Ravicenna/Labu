59575a29677b90a8b106ceb710acef65f35f9d73
israsaqoba <111202315287@mhs.dinus.ac.id>
2025-03-22 02:03:45 +0700

    update

--- app/Views/users/home1.php
+++ app/Views/users/home1.php
@@ -214,50 +214,30 @@
 </section>
 <section>
     <h5 class="display-5 text-center">Daftar Laporan</h5>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php for ($i = 1; $i < 9; $i++) {
+                    echo "<tr>";
+                    echo "<th scope=row>";
+                    echo $i;
+                    echo "</th>";
+                    echo "<td>Mark</td>";
+                    echo "<td>Otto</td>";
+                    echo "<td>@mdo</td>";
+                    echo "</tr>";
+                } ?>
+            </tbody>
+        </table>
+    </div>
 </section>
 
 <br>
